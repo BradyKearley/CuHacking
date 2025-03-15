f242f6293f09870d0bb942350afbc5046a9e0230
Brady Raye Kearley <bradykearley84@gmail.com>
2025-03-15 14:14:17 -0400

    Added Spikes

---
+++ spike.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+var actor: Node2D
+
+func _ready() -> void:
+	$AnimatedSprite2D.play("Up")
+func press(body: Node2D) -> void:
+	# Change if shift signal 
+	actor = body
+	$Timer.start()
+	# test
+
+func release() -> void:
+	actor = null
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		press(body)
+
+func _on_detector_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		release()
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
