f1823fe859e6ff055c03918e7112399b0b2e9ede
Max Sobota <156971303+MaxSobota@users.noreply.github.com>
2025-03-15 13:59:11 -0400

    Added CTRLZ

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -3,12 +3,13 @@ extends RigidBody2D
 var push_force = 50  # Adjust to control push strength
 var push_vector = Vector2.ZERO
 		
-func _on_hitbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		print("touch")
-		if body.is_caps:
-			push_force = 100
-		else: # Small box, small player
-			push_force = 50
-		push_vector = (global_position - body.global_position).normalized()
-		apply_force(Vector2.ZERO, push_vector * push_force)
+#func _on_hitbox_body_entered(body: Node2D) -> void:
+	#if body.is_in_group("Player"):
+		#print("touch")
+		#if body.is_caps:
+			#push_force = 1000
+		#else: # Small box, small player
+			#push_force = 500
+		#push_vector = (global_position - body.global_position).normalized()
+		#print(push_vector)
+		#apply_force(push_vector * push_force, push_vector * push_force)
---
+++ Scripts/ctrlz.gd
@@ -0,0 +1,28 @@
+extends StaticBody2D
+
+var actor: Node2D
+
+func press(body: Node2D) -> void:
+	# Change if shift signal 
+	$AudioStreamPlayer2D.pitch_scale = randf_range(0.8,1.2)
+	$AudioStreamPlayer2D.play()
+	$AnimatedSprite2D.play("Down")
+	actor = body
+	$Timer.start()
+	# test
+
+func release() -> void:
+	$AnimatedSprite2D.play("Up")
+	actor = null
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		press(body)
+
+func _on_detector_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		release()
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
+	print("FUCK")
