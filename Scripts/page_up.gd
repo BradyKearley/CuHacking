e0bd68dbf8742c799c5db9cf7090afa0e7f8812a
Brady Raye Kearley <bradykearley84@gmail.com>
2025-03-15 00:10:47 -0400

    Added Button Ui/Ux

--- Scripts/page_up.gd
+++ Scripts/page_up.gd
@@ -1,26 +1,21 @@
 extends StaticBody2D
 
 # Set textures here maybe
+var player: Node2D
 
-var is_pressed = false
-var old_pos_y
 var jump_height = 300.0
-
-func _ready() -> void:
-	old_pos_y = global_position.y
 
 func press(body: Node2D) -> void:
 	# Change if shift signal 
-	
-	body.velocity.y = -jump_height  # Strong upward push
-	body.move_and_slide()  # Apply the new velocity
-	
-	is_pressed = true
-	global_position.y = old_pos_y + 5
+	$AudioStreamPlayer2D.pitch_scale = randf_range(0.8,1.2)
+	$AudioStreamPlayer2D.play()
+	$AnimatedSprite2D.play("Down")
+	player = body
+	$Timer.start()
 
 func release() -> void:
-	is_pressed = false
-	global_position.y = old_pos_y
+	player = null
+	$AnimatedSprite2D.play("Up")
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):  # Ensure the player is falling onto the button
@@ -29,3 +24,10 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 func _on_detector_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		release()
+
+
+func _on_timer_timeout() -> void:
+	$Timer.wait_time = .1  # Move the player
+	if(player):
+		player.velocity.y = -jump_height  # Strong upward push
+		player.move_and_slide()  # Apply the new velocity
--- Scripts/tab.gd
+++ Scripts/tab.gd
@@ -1,22 +1,18 @@
 extends StaticBody2D
-
+var player: Node2D
 # Set textures here maybe
-
-var is_pressed = false
-var old_pos_y
-
-func _ready() -> void:
-	old_pos_y = global_position.y
-
 func press(body: Node2D) -> void:
 	# Change if shift signal 
-	body.global_position.x += 50 # Maybe play an animation here?
-	is_pressed = true
-	global_position.y = old_pos_y + 5
+	$AudioStreamPlayer2D.pitch_scale = randf_range(0.8,1.2)
+	$AudioStreamPlayer2D.play()
+	$AnimatedSprite2D.play("Down")
+	player = body
+	$Timer.start()
+	
 
 func release() -> void:
-	is_pressed = false
-	global_position.y = old_pos_y
+	$AnimatedSprite2D.play("Up")
+	player = null
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):  # Ensure the player is falling onto the button
@@ -25,3 +21,9 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 func _on_detector_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		release()
+	
+
+func _on_timer_timeout() -> void:
+	$Timer.wait_time = .1  # Move the player
+	if(player):
+		player.global_position.x += 50 # Maybe play an animation here?
