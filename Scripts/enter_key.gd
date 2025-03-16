3c59d2fd7bb0c0bd8bb401170a89a40652491981
Max Sobota <156971303+MaxSobota@users.noreply.github.com>
2025-03-15 21:31:12 -0400

    Fixed level loading

--- Scripts/enter_key.gd
+++ Scripts/enter_key.gd
@@ -1,13 +1,9 @@
 extends Node2D
 
-@export var next_level: PackedScene
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		$AudioStreamPlayer2D.play()
 		$Timer.start()
 
-
 func _on_timer_timeout() -> void:
-	get_tree().change_scene_to_packed(next_level)
-	
+	LevelManager.load_next_level()
---
+++ Scripts/level_manager.gd
@@ -0,0 +1,38 @@
+extends Node
+
+var solo_levels = [
+	"res://Levels/level_one.tscn",
+	"res://Levels/level_two.tscn",
+	"res://Levels/level_two_and_a_half.tscn",
+	"res://Levels/level_three.tscn",
+	"res://Levels/level_four.tscn",
+	"res://Levels/levelfive.tscn",
+	"res://Levels/level_six.tscn",
+	"res://Levels/level_nine.tscn",
+	"res://Levels/level_seven.tscn",
+	"res://Levels/level_ten.tscn",
+	"res://Levels/level_something.tscn"
+]
+
+var duo_levels = [
+	"res://Levels//DuoLevels/duo_level_one.tscn",
+	"res://Levels//DuoLevels/duo_level_two.tscn"
+]
+
+var level_index = 0
+
+func load_next_level():
+	if Global.duo:
+		if level_index < duo_levels.size():
+			get_tree().change_scene_to_file(duo_levels[level_index])
+		else:
+			print("Game Over or Loop Back to First Level")
+			level_index = 0  # Restart game or handle differently
+			get_tree().change_scene_to_file("res://Levels/credits_level.tscn")
+	else:
+		if level_index < solo_levels.size():
+			get_tree().change_scene_to_file(solo_levels[level_index])
+		else:
+			print("Game Over or Loop Back to First Level")
+			level_index = 0  # Restart game or handle differently
+			get_tree().change_scene_to_file("res://Levels/credits_level.tscn")
